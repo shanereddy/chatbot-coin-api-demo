c3b0505f6bca04c134e47ea4360a52c668dd7bcc
Shane Reddy <shane2807@gmail.com>
2018-04-11 17:26:52 +0200

    Added integration into dialogueflow

--- src/index.js
+++ src/index.js
@@ -10,14 +10,27 @@ const app = express()
 app.use(bodyParser.json())
 
 app.use('/price', function(req, res) {
-  coinbaseApi
-    .priceOf('bitcoin')
-    .then(function(response) {
-      res.json(response)
-    })
-    .catch(function(error) {
-      res.json(error)
-    })
+  var responseText = ''
+  const intentName = req.body.result.metadata.intentName
+  console.log('intentName: ' + intentName)
+
+  if (intentName === 'crypto-price') {
+    const coinName = req.body.result.parameters['cryptocurrency-name']
+    coinbaseApi
+      .priceOf(coinName)
+      .then(function(response) {
+        console.log('coinbase response usd: ' + response.coin.usd)
+        responseText = 'The price of your coin is $' + response.coin.usd
+        res.json({speech: responseText, displayText: responseText})
+      })
+      .catch(function(error) {
+        responseText = 'There was an error in retrieving the price of your coin'
+        res.json({speech: responseText, displayText: responseText})
+      })
+  } else {
+    responseText = 'We havent catered for the intent yet!'
+    res.json({speech: responseText, displayText: responseText})
+  }
 })
 
 app.listen(3000, function() {
